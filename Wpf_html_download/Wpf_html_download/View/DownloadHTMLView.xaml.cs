19a4392e27bed2ec421043ca7939f62ae0c979b9
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-08-25 14:48:49 +0200

    Add DownloadHTMLView, Add DownloadHTMLViewModel

---
+++ Wpf_html_download/Wpf_html_download/View/DownloadHTMLView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Wpf_html_download.ViewModel;
+
+namespace Wpf_html_download.View
+{
+    /// <summary>
+    /// Interaction logic for DownloadHTMLView.xaml
+    /// </summary>
+    public partial class DownloadHTMLView : Window
+    {
+        public DownloadHTMLView()
+        {
+            InitializeComponent();
+            this.DataContext = new DownloadHTMLViewModel(this);
+        }
+    }
+}
